d03a8da360d71f3e9289cf678e0f3b7489093c4e
dev-aditya99 <mail.adityasharma99@gmail.com>
2026-03-28 20:44:27 +0530

    Added measurementId to get analytics

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,16 +1,18 @@
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID as string,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APPID as string,
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY as string,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN as string,
-  firestoreDatabaseId: process.env.NEXT_PUBLIC_FIREBASE_FIRESTOREDATABASEID as string,
+  firestoreDatabaseId: process.env
+    .NEXT_PUBLIC_FIREBASE_FIRESTOREDATABASEID as string,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET as string,
-  messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID as string,
- measurementId: ""
+  messagingSenderId: process.env
+    .NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID as string,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string,
 };
 
 // Initialize Firebase SDK
