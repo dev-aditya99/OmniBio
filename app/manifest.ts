0eccc6f641ec6d1e69103863483bacefef7a3265
dev-aditya99 <mail.adityasharma99@gmail.com>
2026-03-28 23:25:20 +0530

    Added cloud links of OmniBio icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,23 +11,23 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#4f46e5',
     icons: [
       {
-        src: '/favicon-16x16.png',
+        src: 'https://ik.imagekit.io/adityazvs6yuayk/logos/favicon-32x32.png',
         sizes: '16x16',
         type: 'image/png',
       },
       {
-        src: '/favicon-32x32.png',
+        src: 'https://ik.imagekit.io/adityazvs6yuayk/logos/favicon-32x32.png',
         sizes: '32x32',
         type: 'image/png',
       },
       {
-        src: '/android-chrome-192x192.png',
+        src: 'https://ik.imagekit.io/adityazvs6yuayk/logos/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable'
       },
       {
-        src: '/android-chrome-512x512.png',
+        src: 'https://ik.imagekit.io/adityazvs6yuayk/logos/android-chrome-512x512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable'
